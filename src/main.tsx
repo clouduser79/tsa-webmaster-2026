663e7309a19527b9138950a7a832da586c76106a
shawnmankotia <shawnmankotia79@gmail.com>
2026-01-26 20:14:29 -0500

    Make Project work with Github Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tsa-webmaster-2026">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tsa-webmaster-2026/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
